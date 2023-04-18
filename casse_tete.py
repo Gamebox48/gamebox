3f3e321ee12294853283f6dda7bac81c81e17dae
Matteo <tropheensi.gamebox@gmail.com>
2023-04-18 13:33:20 +0200

    Commit casse tete Matteo

--- casse_tete.py
+++ casse_tete.py
@@ -2,8 +2,8 @@ from likeprocessing.processing import *
 taillecase = 50
 nb_case_hauteur = 5
 nb_case_largeur = 5
-plateau = [[i*nb_case_largeur+j+1 for j in range(nb_case_largeur)] for i in range(nb_case_hauteur)]
-plateau[-1][-1]=0
+textFont("Comic sans ms",30)
+ihm = IhmScreen()
 
 
 def setup():
@@ -11,12 +11,12 @@ def setup():
     background("grey")
     textAlign("center", "center")
     textFont("arial", 20)
-    melange()
+    init_jeu()
 
 def move(case:tuple):
-    global plateau,nb_case_largeur,nb_case_hauteur
+    global plateau,nb_case_largeur,nb_case_hauteur, fini
     i,j=case
-    if i > nb_case_hauteur or j >nb_case_largeur or i > nb_case_largeur or j > nb_case_hauteur:
+    if i > nb_case_hauteur or j > nb_case_largeur:
         return False
     if i+1 <= nb_case_hauteur-1 and plateau[i+1][j] == 0:
         plateau[i][j],plateau[i+1][j]=plateau[i+1][j],plateau[i][j]
@@ -32,17 +32,19 @@ def move(case:tuple):
         print("la case s'est déplacé à gauche")
     else:
         return False
+    gagne()
     return True
 
 def gagne():
     global fini
     for i in range(nb_case_hauteur):
         for j in range(nb_case_largeur):
-            if plateau[i][j]!=0 and plateau[i][j] != i*nb_case_largeur+j+i:
+            if plateau[i][j] != i*nb_case_largeur+j+1 and plateau[i][j] != 0:
                 return False
     fini=2
     ihm.objet_by_name('bouton_recommencer_fin').visible = True
     ihm.objet_by_name('bouton_menu_fin').visible = True
+    ihm.objet_by_name('bouton_pause').visible = False
     return True
 
 
@@ -71,16 +73,29 @@ def reprendre():
     ihm.objet_by_name('bouton_menu_pause').visible = False
     fini=0
 
-ihm.addObjet(Bouton(ihm, (75, 250, 100, 100), '||', command=pause), 'bouton_pause')
-ihm.addObjet(Bouton(ihm, (250//2, 350, 150, 50), '||', command=reprendre), 'bouton_reprendre')
+def init_jeu():
+    global plateau, fini
+    plateau = [[i * nb_case_largeur + j + 1 for j in range(nb_case_largeur)] for i in range(nb_case_hauteur)]
+    plateau[-1][-1] = 0
+    fini=0
+    ihm.objet_by_name('bouton_pause').visible = True
+    ihm.objet_by_name('bouton_reprendre').visible = False
+    ihm.objet_by_name('bouton_recommencer_pause').visible = False
+    ihm.objet_by_name('bouton_menu_pause').visible = False
+    ihm.objet_by_name('bouton_recommencer_fin').visible = False
+    ihm.objet_by_name('bouton_menu_fin').visible = False
+
+
+ihm.addObjet(Bouton(ihm, (110, 310, 30, 30), '||', command=pause), 'bouton_pause')
+ihm.addObjet(Bouton(ihm, (110, 310, 30, 30), '||', command=reprendre), 'bouton_reprendre')
 ihm.objet_by_name('bouton_reprendre').visible = False
-ihm.addObjet(Bouton(ihm, (0, 350, 150, 50), 'Recommencer', command=reprendre), 'bouton_recommencer_pause')
+ihm.addObjet(Bouton(ihm, (0,300,110 ,50), 'Recommencer', command=init_jeu), 'bouton_recommencer_pause')
 ihm.objet_by_name('bouton_recommencer_pause').visible = False
-ihm.addObjet(Bouton(ihm, (0, 350, 150, 50), 'Retour au menu', command=reprendre), 'bouton_menu_pause')
+ihm.addObjet(Bouton(ihm, (140, 300, 110, 50), 'Retour au menu', command=reprendre), 'bouton_menu_pause')
 ihm.objet_by_name('bouton_menu_pause').visible = False
-ihm.addObjet(Bouton(ihm, (0, 350, 150, 50), 'Recommencer', command=reprendre), 'bouton_recommencer_fin')
+ihm.addObjet(Bouton(ihm, (0, 300, 125, 50), 'Recommencer', command=init_jeu), 'bouton_recommencer_fin')
 ihm.objet_by_name('bouton_recommencer_fin').visible = False
-ihm.addObjet(Bouton(ihm, (0, 350, 150, 50), 'Retour au menu', command=reprendre), 'bouton_menu_fin')
+ihm.addObjet(Bouton(ihm, (125, 300, 125, 50), 'Retour au menu', command=reprendre), 'bouton_menu_fin')
 ihm.objet_by_name('bouton_menu_fin').visible = False
 
 fini=0
@@ -88,14 +103,18 @@ fini=0
 def compute():
     if mouse_button_pressed() == 0:
         i, j = ((mouseY() // taillecase), (mouseX() // taillecase))
-        if i <= nb_case_largeur-1 and j <= nb_case_hauteur-1:
-            move((i, j))
+        if fini == 0:
+            if i <= nb_case_largeur-1 and j <= nb_case_hauteur-1:
+                move((i, j))
+    ihm.scan_events()
 
 def draw():
     for j in range(nb_case_largeur):
         for i in range(nb_case_hauteur):
             if plateau[j][i] != 0:
                 text(str(plateau[j][i]), taillecase * i, taillecase * j, taillecase, taillecase)
-    draw_
+    if fini ==2:
+        text('Bravo, vous avez gagné!', 0, 250, 250, 50)
+    ihm.draw()
 
 run(globals())
