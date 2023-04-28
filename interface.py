1a5c986286ddfeeac3cfe9d42a15eece7698d040
Matteo <tropheensi.gamebox@gmail.com>
2023-04-28 14:38:12 +0200

    pendu in dialog integré à l'interface

--- interface.py
+++ interface.py
@@ -4,12 +4,13 @@ from casse_tete_in_dialog import CasseTete
 from memory.memory_file import Memory
 from morpion_in_dialog import Morpion
 from sudoku_in_dialog import Sudoku
+from pendu_in_dialog import Pendu
 from time import sleep
 # from sudoku.sudoku import Sudoku
 
-jeux = ['Morpion', 'Puissance 4', 'Snake', 'Démineur', 'Casse-tête', 'Pendu', 'Memory', 'Sudoku']
+jeux = ['Morpion', 'Puissance 4', 'Snake', 'Casse-tête', 'Pendu', 'Memory', 'Sudoku']
 img = [loadImage('img/Morpion.jpg'), loadImage('img/Puissance4.jpg'), loadImage('img/Snake.jpg'),
-       loadImage('img/Démineur.jpg'), loadImage('img/Casse_tete.jpg'), loadImage('img/Pendu.jpg'),
+       loadImage('img/Casse_tete.jpg'), loadImage('img/Pendu.jpg'),
        loadImage('img/Memory.jpg'), loadImage('img/Sudoku.jpg')]
 fond = loadImage('img/logo.png')
 boite = loadImage('img/boite.jpg')
@@ -32,24 +33,29 @@ def click(name):
             ihm.addObjet(Morpion(ihm, 620, 100), "morpion")
             index_jeu=0
             jeu_en_cours = True
-        if name == 2:
+        elif name == 2:
             index_jeu = 2
             Snake.init_snake()
             jeu_en_cours = True
-        elif name == 4:
+        elif name == 3:
             ihm.init()
             ihm.addObjet(CasseTete(ihm, 620, 100), "casse_tete")
+            index_jeu = 3
+            jeu_en_cours = True
+        elif name == 4:
+            ihm.init()
+            ihm.addObjet(Pendu(ihm, 400, 0), "pendu")
             index_jeu = 4
+            jeu_en_cours = True
+        elif name == 5:
+            ihm.init()
+            ihm.addObjet(Memory(ihm, 375, 0), "memory")
+            index_jeu = 5
             jeu_en_cours = True
         elif name == 6:
             ihm.init()
-            ihm.addObjet(Memory(ihm, 375, 0), "memory")
-            index_jeu = 6
-            jeu_en_cours = True
-        elif name == 7:
-            ihm.init()
             ihm.addObjet(Sudoku(ihm, 550, 15), "sudoku")
-            index_jeu = 7
+            index_jeu = 6
             jeu_en_cours = True
 
 
@@ -92,32 +98,46 @@ def compute():
                 jeu_en_cours = False
                 index_jeu = -2
         Snake.compute()
-    elif index_jeu == 4:
+    elif index_jeu == 3:
         # est ce la fin du jeu casse tete
         if ihm.objet_by_name("casse_tete").destroy:
             jeu_en_cours = False
         if ihm.objet_by_name("casse_tete").fini == 2:
             if AskYesNo(ihm, "Voulez-vous\nrejouer").response() == 0:
-                index_jeu = 4
+                index_jeu = 3
                 ihm.addObjet(CasseTete(ihm, 620, 100), "casse_tete")
                 jeu_en_cours = True
             else:
                 jeu_en_cours = False
                 index_jeu = -2
                 ihm.init()
-    elif index_jeu == 6:
+    elif index_jeu == 4:
+        # calculs et fin du jeu pendu
+        if ihm.objet_by_name("pendu").destroy:
+            jeu_en_cours = False
+        if ihm.objet_by_name("pendu").gagne ==  True or ihm.objet_by_name("pendu").perdu == True:
+            sleep(2)
+            if AskYesNo(ihm, "Voulez-vous\nrejouer").response() == 0:
+                index_jeu = 4
+                ihm.addObjet(Pendu(ihm, 400, 0), "pendu")
+                jeu_en_cours = True
+            else:
+                jeu_en_cours = False
+                index_jeu = -2
+                ihm.init()
+    elif index_jeu == 5:
         # calculs et fin du jeu memory
         ihm.objet_by_name("memory").compute()
         if ihm.objet_by_name("memory").destroy:
             jeu_en_cours = False
-    elif index_jeu==7:
+    elif index_jeu==6:
         # calculs et fin du jeu sudoku
         if ihm.objet_by_name("sudoku").destroy:
             jeu_en_cours = False
         if ihm.objet_by_name("sudoku").fini == 4 or ihm.objet_by_name("sudoku").fini == 5:
             sleep(2)
             if AskYesNo(ihm, "Voulez-vous\nrejouer").response() == 0:
-                index_jeu = 7
+                index_jeu = 6
                 ihm.addObjet(Sudoku(ihm, 400, 0), "sudoku")
                 jeu_en_cours = True
             else:
